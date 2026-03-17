2e989b32376fbeb3b925bcc7b7b8b8d02b08fdd8
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-03-17 12:02:48 +0100

    Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+const actress_api = "https://lanciweb.github.io/demo/api/actresses/"
+const actors_api = "https://lanciweb.github.io/demo/api/actors/"
+
 
 function App() {
- 
+  const [renderActress, setRenderActress] = useState([])
+  
+  function actressFunction() {
+    fetch(actress_api)
+      .then(res => res.json())
+      .then(data => {
+        setRenderActress(data)
+        console.log(data);
+      })
+
+  }
+  useEffect(actressFunction, [])
+
+  const [renderActor, setRenderActor]=useState([])
+
+  function actorsFunction(){
+    fetch(actors_api)
+    .then(res => res.json())
+    .then(data => {
+      setRenderActor(data)
+      console.log(data);
+      
+    })
+  }
+  useEffect(actorsFunction,[])
 
   return (
     <>
-     
+
     </>
   )
 }
