7087d4d8efe3aa98faa65be6ab9e4e06426ea0fa
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-03-17 12:18:25 +0100

    Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const actors_api = "https://lanciweb.github.io/demo/api/actors/"
 
 function App() {
   const [renderActress, setRenderActress] = useState([])
-  
+
   function actressFunction() {
     fetch(actress_api)
       .then(res => res.json())
@@ -18,21 +18,44 @@ function App() {
   }
   useEffect(actressFunction, [])
 
-  const [renderActor, setRenderActor]=useState([])
+  const [renderActor, setRenderActor] = useState([])
 
-  function actorsFunction(){
+  function actorsFunction() {
     fetch(actors_api)
-    .then(res => res.json())
-    .then(data => {
-      setRenderActor(data)
-      console.log(data);
-      
-    })
+      .then(res => res.json())
+      .then(data => {
+        setRenderActor(data)
+        console.log(data);
+
+      })
   }
-  useEffect(actorsFunction,[])
+  useEffect(actorsFunction, [])
 
   return (
     <>
+      <div className="section">
+        <div className="container">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+            {
+              renderActress.map((actress,id) => (
+                <div key={actress.id} className="col">
+                  <div className="card">
+                    <img src={actress.image} alt="" />
+                    <div className="card-body">
+                      <h4>{actress.name}</h4>
+                      <div><span>Birth Year: {actress.birth_year}</span></div>
+                      <div><span>Nationality: {actress.nationality}</span></div>
+                      <div><span>Biography: {actress.biography}</span></div>
+                      <div><span>Awards: {actress.awards}</span></div>
+                    </div>
+                  </div>
+                </div>
+                
+               ))
+            }
+          </div>
+        </div>
+      </div>
 
     </>
   )
